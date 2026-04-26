e548026a2e4e47700bd4c841fea6436d3030b946
redafrix <redafrix2002@gmail.com>
2026-04-26 18:23:53 +0200

    FIX: Use relative base path for better compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/reda-portfolio/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
+  base: './',
 })
